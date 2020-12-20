36815fbc74c7d5ef3c434a3058fdfd3aee4c30a7
Julian Pufler <puf17640@spengergasse.at>
2020-12-20 20:42:14 +0100

    mongoose

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const	createPost = require('./util'),
 	Instagram = require('instagram-web-api')
 	path = require('path'),
 	{ readFile } = require('fs'),
-	cron = require('cron')
+	cron = require('cron'),
+	mongoose = require('mongoose')
 
 const result = require('dotenv').config()
 result.error && console.error(result.error)
@@ -14,26 +15,15 @@ const alreadyReplied = [
 	'18048818101279278',
 	'17851021136430423'
 ]
-/*
-async function login() { 
-	const client = new IgApiClient()
-	if (process.env.IG_USERNAME && process.env.IG_PASSWORD) {
-		try {
-			client.state.generateDevice(process.env.IG_USERNAME)
-			await client.simulate.preLoginFlow()
-			const user = await client.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD)
-			process.nextTick(async () => await client.simulate.postLoginFlow())
-			console.info('Instagram login success.\n')
-			return { client, user }
-		} catch (error) {
-			console.error('Instagram login fail: ' + error)
-			process.exit(-1)
-		}
-	} else {
-		throw Error('Instagram username and password are required to be set in the environment.')
-	}
-}
-*/
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true,  useUnifiedTopology: true})
+mongoose.connection.on('error', console.error.bind(console, 'Connection error:'))
+
+const Reply = mongoose.model("Reply", mongoose.Schema({
+	mentionId: Number,
+	replyId: Number
+}));
+
 const client = new Instagram({ username: process.env.IG_USERNAME, password: process.env.IG_PASSWORD })
 
 client.login().then(async () => {
@@ -44,8 +34,7 @@ client.login().then(async () => {
 			console.info(`Starting post.`)
 			createPost().then(async (caption) => {
 				const post = await client.uploadPhoto({ photo: path.join(__dirname, 'temp.jpeg'), caption, post: 'feed' })
-				console.log(post)
-				console.info(`Post status: ${post.status} (${post.id})`)
+				console.info(`Post status: ${post.status}`)
 			})
 		}catch(err) {
 			console.error(err)
@@ -53,12 +42,12 @@ client.login().then(async () => {
 	}).start()
 	cron.job('0 * * * * *', async () => {
 		try {
-			console.info('Searching for mentions.')
 			const mentions = (await client.getActivity()).activity_feed.edge_web_activity_feed.edges
 				.map(e => e.node).filter(n => n.type === 5)
 			mentions.forEach(async (mention) => {
 				const commentId = mention.id.split('_')[1] 
-				if(alreadyReplied.includes(commentId))
+				const reply = await Reply.findOne({ mentionId: commentId }).exec()
+				if(reply)
 					return
 				const caption = await createPost(mention.media.thumbnail_src)
 				const comment = await client.addComment({ 
@@ -66,11 +55,12 @@ client.login().then(async () => {
 					text: caption, 
 					replyToCommentId: commentId
 				})
-				console.info(`Comment status: ${comment.status} (${comment.id})`)
-				alreadyReplied.push(commentId)
+				const mentionReply = await new Reply({ mentionId: commentId, replyId: comment.id }).save()
+				console.info(`Comment status: ${comment.status} (${mentionReply.replyId})`)
 			})
 		}catch(err) {
-			console.error(err)
+			console.error(err.message)
+			await new Reply({ mentionId: commentId, errorMessage: err.message }).save()
 		}
 	}).start()
 })
